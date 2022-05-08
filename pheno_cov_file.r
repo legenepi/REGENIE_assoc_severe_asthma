febeb1d87ba472bd298c58e5a9acbd812c30ad66
piganoemi <nnp5@leicester.ac.uk>
2022-05-08 18:41:15 +0100

    update pheno cov script

--- pheno_cov_file.r
+++ pheno_cov_file.r
@@ -12,29 +12,24 @@ demo_file <- paste0(path_prefix,"demographics.txt")
 
 demo <- fread(demo_file,sep="\t")
 
-demo$pheno_all_age <- "NA"
-
-demo <- demo %>% mutate(pheno_all_age = ifelse(demo$modsev_asthma == 1, 1,
-ifelse(demo$controls == 1, 0, "NA")))
-
-demo <- demo %>% mutate(pheno_all_age = case_when(modsev_asthma == 1 ~ 1
-                                                 ,controls == 1 ~ 1
-                                                 , TRUE ~ 'NA')
+demo <- demo %>%
+  mutate(pheno_allasthma_all_age = case_when(asthma_diagnosis == 1 ~ 1,
+                                controls == 1                ~ 0,
+                                TRUE                         ~ as.numeric('NA')))
 
 demo <- demo %>%
-  mutate(pheno_modsev_all_age = case_when(modsev_asthma == 1 ~ 1
-                                ,controls == 1 ~ 0
-                                ,TRUE ~ as.numeric(pheno_all_age)
-                                )
-  )
+  mutate(pheno_modsev_all_age = case_when(modsev_asthma == 1 ~ 1,
+                                controls == 1                ~ 0,
+                                TRUE                         ~ as.numeric('NA')))
+
 
 demo <- demo %>%
-  mutate(pheno_modsev_adult = ifelse(demo$pheno_modsev_all_age == 1 & demo$category_onset == 'adult_onset', 1,
+  mutate(pheno_modsev_adult = ifelse(demo$pheno_modsev_all_age == 1 & demo$category_onset == 'onset_adult', 1,
   ifelse(demo$pheno_modsev_all_age == 0, 0, as.numeric(demo$pheno_modsev_adult))))
 
 
 demo <- demo %>%
-  mutate(pheno_modsev_early = ifelse(demo$pheno_modsev_all_age == 1 & demo$category_onset == 'early_onset', 1,
+  mutate(pheno_modsev_early = ifelse(demo$pheno_modsev_all_age == 1 & demo$category_onset == 'onset_early', 1,
   ifelse(demo$pheno_modsev_all_age == 0, 0, as.numeric(demo$pheno_modsev_early))))
 
 demo %>% filter(pheno_modsev_all_age == 1) %>% count(clustered.ethnicity)
@@ -54,5 +49,5 @@ demo_EUR_pheno_cov <- demo_EUR %>% select(FID,
                                           everything())
 
 write.table(demo_EUR_pheno_cov,paste0(output_prefix,"demo_EUR_pheno_cov.txt"),
-row.names = FALSE, col.names = TRUE ,quote=FALSE, sep=" ")
+row.names = FALSE, col.names = TRUE ,quote=FALSE, sep=" ", na = "NA")
 
