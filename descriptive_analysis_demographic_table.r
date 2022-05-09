58c37ee54cfa4a4a04b824c1be5e757860b3e8b6
piganoemi <nnp5@leicester.ac.uk>
2022-05-09 13:23:57 +0100

    update descriptive analysis

--- descriptive_analysis_demographic_table.r
+++ descriptive_analysis_demographic_table.r
@@ -3,19 +3,43 @@
 #upload required libraries
 library(data.table)
 library(tidyverse)
-library(pastecs)
+library(plotrix)
 
 #load input:
-path_prefix <- "/home/n/nnp5/PhD/PhD_project/UKBiobank_datafields/data/"
+path_prefix <- "/home/n/nnp5/PhD/PhD_project/REGENIE_assoc/data/"
 
-demo_file <- paste0(path_prefix,"demographics.txt")
+demo_file <- paste0(path_prefix,"demo_pheno_cov.txt")
 
-demo <- fread(demo_file,sep="\t")
+demo <- fread(demo_file,fill=TRUE)
 
-#remove any participant neither case nor control:
-qc_demo <- demo %>% filter(pheno_)
+#make eid, genetic_sex, pheno_modsev_all_age categorical variables:
+demo$eid <- as.factor(demo$eid)
+demo$genetic_sex <- as.factor(demo$genetic_sex)
+demo$pheno_modsev_all_age <- as.factor(demo$pheno_modsev_all_age)
+#make age_onset_merge numeric:
+demo$age_onset_merge <- as.numeric(demo$age_onset_merge)
+
+
+#remove any participant neither case nor control for pheno_modsev_all_age
+modsev_demo <- demo %>% filter(!is.na(pheno_modsev_all_age))
 
 #case/control and male/female subdivision
 
 #quantitative traits:
-age_at_recruitment
+modsev_demo_quantitative <- modsev_demo %>% select(eid, genetic_sex, pheno_modsev_all_age,
+                                                            age_at_recruitment,
+                                                            BMI,
+                                                            age_onset_merge,
+                                                            best_FEV1,
+                                                            perc_pred_FEV1,
+                                                            ratio_FEV1_FVC,
+                                                            cigarette_pack_years
+                                                            )
+
+for (col in colnames(modsev_demo_quantitative)[-c(1,2,3)]){
+print(paste0("column:",col))
+print(paste0("N valid values:",length(na.omit(modsev_demo_quantitative[[col]]))))
+print(paste0("mean:",mean(modsev_demo_quantitative[[col]],na.rm=TRUE)))
+print(paste0("std error:",std.error(modsev_demo_quantitative[[col]],na.rm)))
+print("-------------------------------")
+}
