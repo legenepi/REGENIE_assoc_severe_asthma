0e48372f2f22247c4ff7f5079636bbd699d5b7ca
piganoemi <nnp5@leicester.ac.uk>
2022-05-09 17:39:42 +0100

    plots in descriptive analysis

--- descriptive_analysis_demographic_table.r
+++ descriptive_analysis_demographic_table.r
@@ -4,6 +4,7 @@
 library(data.table)
 library(tidyverse)
 library(plotrix)
+library(rcartocolor)
 
 
 #function to find N valid values, mean and standard error for a quantitative variable in a dataset:
@@ -21,7 +22,7 @@ path_prefix <- "/home/n/nnp5/PhD/PhD_project/REGENIE_assoc/data/"
 
 demo_file <- paste0(path_prefix,"demo_pheno_cov.txt")
 
-demo <- fread(demo_file,fill=TRUE)
+demo <- read.table(demo_file,sep= " ",header=TRUE)
 
 #make eid, genetic_sex, pheno_modsev_all_age categorical variables:
 demo$eid <- as.factor(demo$eid)
@@ -33,8 +34,6 @@ demo$age_onset_merge <- as.numeric(demo$age_onset_merge)
 
 #remove any participant neither case nor control for pheno_modsev_all_age
 modsev_demo <- demo %>% filter(!is.na(pheno_modsev_all_age))
-
-#case/control and male/female subdivision
 
 #modsev descriptive:
 modsev_demo_quantitative <- modsev_demo %>% select(eid, genetic_sex, pheno_modsev_all_age,
@@ -52,15 +51,62 @@ quant_descr(modsev_demo_quantitative[,-c(1,2,3)])
 #case modsev dataset:
 case_modsev_demo_quantitative <- modsev_demo_quantitative %>% filter(pheno_modsev_all_age == 1)
 quant_descr(case_modsev_demo_quantitative[,-c(1,2,3)])
+
 female_case_modsev_demo_quantitative <- case_modsev_demo_quantitative %>% filter(genetic_sex == "0")
 quant_descr(female_case_modsev_demo_quantitative[,-c(1,2,3)])
+
 male_case_modsev_demo_quantitative <- case_modsev_demo_quantitative %>% filter(genetic_sex == "1")
 quant_descr(male_case_modsev_demo_quantitative[,-c(1,2,3)])
+
 
 #control modsev dataset:
 control_modsev_demo_quantitative <- modsev_demo_quantitative %>% filter(pheno_modsev_all_age == 0)
 quant_descr(control_modsev_demo_quantitative[,-c(1,2,3)])
+
 female_control_modsev_demo_quantitative <- control_modsev_demo_quantitative %>% filter(genetic_sex == "0")
 quant_descr(female_control_modsev_demo_quantitative[,-c(1,2,3)])
+
 male_control_modsev_demo_quantitative <- control_modsev_demo_quantitative %>% filter(genetic_sex == "1")
 quant_descr(male_control_modsev_demo_quantitative[,-c(1,2,3)])
+
+
+#qualitative:
+table(modsev_demo$pheno_modsev_all_age, modsev_demo$clustered.ethnicity)
+table(modsev_demo$pheno_modsev_all_age, modsev_demo$ethnic_background)
+table(modsev_demo$pheno_modsev_all_age, modsev_demo$smoking_status)
+
+table(case_modsev_demo$pheno_modsev_all_age, case_modsev_demo$clustered.ethnicity)
+table(case_modsev_demo$pheno_modsev_all_age, case_modsev_demo$ethnic_background)
+table(case_modsev_demo$pheno_modsev_all_age, case_modsev_demo$smoking_status)
+
+table(female_case_modsev_demo$pheno_modsev_all_age, female_case_modsev_demo$clustered.ethnicity)
+table(female_case_modsev_demo$pheno_modsev_all_age, female_case_modsev_demo$ethnic_background)
+table(female_case_modsev_demo$pheno_modsev_all_age, female_case_modsev_demo$smoking_status)
+
+table(male_case_modsev_demo$pheno_modsev_all_age, male_case_modsev_demo$clustered.ethnicity)
+table(male_case_modsev_demo$pheno_modsev_all_age, male_case_modsev_demo$ethnic_background)
+table(male_case_modsev_demo$pheno_modsev_all_age, male_case_modsev_demo$smoking_status)
+
+##same for control, but look again to fin a smarter way to do this. Maybe use barplot ?
+
+
+
+
+ggplot(modsev_demo %>% filter(clustered.ethnicity != "", clustered.ethnicity != "NA")) +
+  aes(x = clustered.ethnicity, fill = clustered.ethnicity) +
+  geom_bar() +
+  xlab("Genetic ancestry") +
+  ylab("Count") +
+  theme_light() +
+  scale_fill_carto_d(palette = "Earth") +
+  theme(legend.position = "none")
+ggsave(paste0(path_prefix,"genetic_ancestry_modsev_allage.png"))
+
+#PCA plot:
+TO BE MODIFIED
+pca12_plot <- ggplot(modsev_demo, aes(PC1, PC2, col= clustered.ethnicity, shape = pheno_modsev_all_age))
+pca12_plot <- pca12_plot + scale_color_manual(values = c("#A16928","#c29b64","#e0cfa2","#cbd5bc","#85adaf","#2887a1"))
+pca12_plot <- pca12_plot + theme_light()
+pca12_plot <- pca12_plot + xlab("PC1") + ylab("PC2")
+pca12_plot + ggtitle(paste0("PCs plot modsev asthma"))
+#ggsave(paste0(path_prefix,"PCA12_plot_modsev_allage.png"))
