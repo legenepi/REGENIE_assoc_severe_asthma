7a86002bb6a90fdf3f9399d44c21490152752ac8
piganoemi <nnp5@leicester.ac.uk>
2022-05-09 15:31:11 +0100

    update descirptive analysis

--- descriptive_analysis_demographic_table.r
+++ descriptive_analysis_demographic_table.r
@@ -5,6 +5,17 @@ library(data.table)
 library(tidyverse)
 library(plotrix)
 
+
+#function to find N valid values, mean and standard error for a quantitative variable in a dataset:
+##NB: dataset needs to have quantitative variable only !
+quant_descr <- function(dataset) {
+for (col in colnames(dataset)){
+print(paste0("column:",col))
+print(paste0("N valid values:",length(na.omit(dataset[[col]]))))
+print(paste0("mean:",mean(dataset[[col]],na.rm=TRUE)))
+print(paste0("std error:",std.error(dataset[[col]],na.rm)))
+print("-------------------------------")}}
+
 #load input:
 path_prefix <- "/home/n/nnp5/PhD/PhD_project/REGENIE_assoc/data/"
 
@@ -25,7 +36,7 @@ modsev_demo <- demo %>% filter(!is.na(pheno_modsev_all_age))
 
 #case/control and male/female subdivision
 
-#quantitative traits:
+#modsev descriptive:
 modsev_demo_quantitative <- modsev_demo %>% select(eid, genetic_sex, pheno_modsev_all_age,
                                                             age_at_recruitment,
                                                             BMI,
@@ -35,11 +46,21 @@ modsev_demo_quantitative <- modsev_demo %>% select(eid, genetic_sex, pheno_modse
                                                             ratio_FEV1_FVC,
                                                             cigarette_pack_years
                                                             )
+#all modsev dataset:
+quant_descr(modsev_demo_quantitative[,-c(1,2,3)])
 
-for (col in colnames(modsev_demo_quantitative)[-c(1,2,3)]){
-print(paste0("column:",col))
-print(paste0("N valid values:",length(na.omit(modsev_demo_quantitative[[col]]))))
-print(paste0("mean:",mean(modsev_demo_quantitative[[col]],na.rm=TRUE)))
-print(paste0("std error:",std.error(modsev_demo_quantitative[[col]],na.rm)))
-print("-------------------------------")
-}
+#case modsev dataset:
+case_modsev_demo_quantitative <- modsev_demo_quantitative %>% filter(pheno_modsev_all_age == 1)
+quant_descr(case_modsev_demo_quantitative[,-c(1,2,3)])
+female_case_modsev_demo_quantitative <- case_modsev_demo_quantitative %>% filter(genetic_sex == "0")
+quant_descr(female_case_modsev_demo_quantitative[,-c(1,2,3)])
+male_case_modsev_demo_quantitative <- case_modsev_demo_quantitative %>% filter(genetic_sex == "1")
+quant_descr(male_case_modsev_demo_quantitative[,-c(1,2,3)])
+
+#control modsev dataset:
+control_modsev_demo_quantitative <- modsev_demo_quantitative %>% filter(pheno_modsev_all_age == 0)
+quant_descr(control_modsev_demo_quantitative[,-c(1,2,3)])
+female_control_modsev_demo_quantitative <- control_modsev_demo_quantitative %>% filter(genetic_sex == "0")
+quant_descr(female_control_modsev_demo_quantitative[,-c(1,2,3)])
+male_control_modsev_demo_quantitative <- control_modsev_demo_quantitative %>% filter(genetic_sex == "1")
+quant_descr(male_control_modsev_demo_quantitative[,-c(1,2,3)])
