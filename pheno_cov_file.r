4a9086b236bd930be391833e518cca63aad28ec4
piganoemi <nnp5@leicester.ac.uk>
2022-07-25 12:59:05 +0100

    update with right severe cases phenocov

--- pheno_cov_file.r
+++ pheno_cov_file.r
@@ -5,36 +5,35 @@ library(data.table)
 library(tidyverse)
 
 #load input:
-path_prefix <- "/home/n/nnp5/PhD/PhD_project/UKBiobank_datafields/data/"
+path_prefix_1 <- "/home/n/nnp5/PhD/PhD_project/UKBiobank_datafields/data/"
+path_prefix_2 <- "/home/n/nnp5/PhD/PhD_project/UKBiobank_asthmaMeds_stratification/data/"
 output_prefix <- "/home/n/nnp5/PhD/PhD_project/REGENIE_assoc/data/"
 
-demo_file <- paste0(path_prefix,"demographics.txt")
+demo_file <- paste0(path_prefix_1,"demographics.txt")
 
 demo <- fread(demo_file,sep="\t")
 
-demo <- demo %>%
-  mutate(pheno_allasthma_all_age = case_when(asthma_diagnosis == 1 ~ 1,
-                                controls == 1                ~ 0,
-                                TRUE                         ~ as.numeric('NA')))
+severe <- fread(paste0(path_prefix_2,"Eid_severe_intersection"),header=F)
+colnames(severe)[1] <- "eid"
+severe$cases <- as.factor("1")
+
+demo <- left_join(demo,severe,by="eid")
+
+
+demo <- demo %>% mutate(pheno = case_when(controls == 1 ~ 0, cases == 1 ~ 1))
+demo <- demo%>% filter(!is.na(pheno))
+demo$pheno <- as.factor(demo$pheno)
 
 demo <- demo %>%
-  mutate(pheno_modsev_all_age = case_when(modsev_asthma == 1 ~ 1,
-                                controls == 1                ~ 0,
-                                TRUE                         ~ as.numeric('NA')))
+  mutate(pheno_adult = ifelse(demo$pheno == 1 & demo$category_onset == 'onset_adult', 1, as.numeric(demo$pheno_adult)))
 
 
 demo <- demo %>%
-  mutate(pheno_modsev_adult = ifelse(demo$pheno_modsev_all_age == 1 & demo$category_onset == 'onset_adult', 1,
-  ifelse(demo$pheno_modsev_all_age == 0, 0, as.numeric(demo$pheno_modsev_adult))))
+  mutate(pheno_early = ifelse(demo$pheno == 1 & demo$category_onset == 'onset_early', 1, as.numeric(demo$pheno_modsev_early)))
 
-
-demo <- demo %>%
-  mutate(pheno_modsev_early = ifelse(demo$pheno_modsev_all_age == 1 & demo$category_onset == 'onset_early', 1,
-  ifelse(demo$pheno_modsev_all_age == 0, 0, as.numeric(demo$pheno_modsev_early))))
-
-demo %>% filter(pheno_modsev_all_age == 1) %>% count(clustered.ethnicity)
-demo %>% filter(pheno_modsev_adult == 1) %>% count(clustered.ethnicity)
-demo %>% filter(pheno_modsev_early == 1) %>% count(clustered.ethnicity)
+demo %>% filter(pheno == 1) %>% count(clustered.ethnicity)
+demo %>% filter(pheno_adult == 1) %>% count(clustered.ethnicity)
+demo %>% filter(pheno_early == 1) %>% count(clustered.ethnicity)
 
 
 
