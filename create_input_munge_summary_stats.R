3c42d9aeaaed2bd52819812b25eb1ba95cba1695
piganoemi <nnp5@leicester.ac.uk>
2022-08-09 17:06:33 +0100

    minor changes

--- create_input_munge_summary_stats.R
+++ create_input_munge_summary_stats.R
@@ -6,8 +6,6 @@ library(tidyverse)
 args = commandArgs(trailingOnly=TRUE)
 df_file <- args[1]
 pheno <- as.character(args[2])
-cases_n <- as.numeric(args[3])
-controls_n <- as.numeric(args[4])
 
 df <- fread(df_file,header=TRUE)
 
@@ -16,17 +14,12 @@ df <- fread(df_file,header=TRUE)
 
 dfclean <- df %>% select(CHROM,ID,GENPOS,ALLELE1,ALLELE0,A1FREQ,LOG10P,BETA,SE)
 
-dfclean$pval <- 10^(dfclean$LOG10P)
-
-#k:cases_proportion:
-dfclean$k <- cases_n/controls_n
-dfclean$'LOG_ODDS' <- dfclean$BETA/(dfclean$k*(1-dfclean$k))
-dfclean$'se_LOG_ODDS' <- dfclean$SE/(dfclean$k*(1-dfclean$k))
+dfclean$pval <- 10^(-dfclean$LOG10P)
 
 #select column in order:
-dfclean <- dfclean %>% select(ID,CHROM,GENPOS,ALLELE1,ALLELE0,LOG_ODDS,se_LOG_ODDS,A1FREQ,LOG10P)
+dfclean <- dfclean %>% select(ID,CHROM,GENPOS,ALLELE1,ALLELE0,BETA,SE,A1FREQ,pval)
 
 #rename columns :
 colnames(dfclean) <- c("snpid", "b37chr", "bp", "a1", "a2", "LOG_ODDS", "se", "eaf", "pval")
 
-write.table(dfclean,paste0("output/",pheno,"_input_mungestat"),quote=FALSE,sep="\t",row.names=FALSE)
+write.table(dfclean,paste0("output/",pheno,"_betase_input_mungestat"),quote=FALSE,sep="\t",row.names=FALSE)
